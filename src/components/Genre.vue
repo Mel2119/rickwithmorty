e2faa711d1ccef3f17a8671e767d1bc092ef8144
Melat Mekonnen <melatlalibela21@gmail.com>
2024-05-28 14:33:59 +0300

    eightht commmit

--- src/components/Genre.vue
+++ src/components/Genre.vue
@@ -1,42 +1,31 @@
 <template>
-<section class="container mx-auto px-4 bg-slate-300">
-    <div class="container flex justify-center text-3xl font-serif mb-8 text-black text-bold  ">
-                <h1>Genre</h1>
-                
+  <section class="container mx-auto px-4 bg-slate-300">
+    <div class="container flex justify-center text-3xl font-serif mb-8 text-black text-bold">
+      <h1>Genre</h1>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-40">
+
+      <div class="card group w-full md:w-56 bg-[#63d72d] h-48 rounded-lg shadow-md overflow-hidden ml-28 hover:transform hover:translate-y-[-30px]">
+        <div class="p-4">
+          <h2 class="text-xl font-bold text-white text-center mt-16">Satire</h2>
         </div>
-  <div class="grid grid-cols-1 md:grid-cols-3   gap-40">
-    
-    <div  class="card w-full   md:w-56 bg-[#63d72d]  h-48 rounded-lg shadow-md overflow-hidden  ml-28 ">
-      
-      <div class="p-4">
-        <h2 class="text-xl font-bold text-white text-center mt-16">Satire</h2>
-        
       </div>
-    </div>
-    <div class="card w-full md:w-56 ml-16 bg-[#63d72d] rounded-lg shadow-md overflow-hidden">
-      
-      <div class="p-4">
-        <h2 class="text-xl font-bold text-white mt-16 text-center">Scientific</h2>
-       
+
+      <div class="card group w-full md:w-56 ml-16 bg-[#63d72d] rounded-lg shadow-md overflow-hidden hover:transform hover:translate-y-[-30px]"> 
+        <div class="p-4">
+          <h2 class="text-xl font-bold text-white mt-16 text-center">Scientific</h2>
+        </div>
       </div>
+
+      <div class="card group w-full md:w-56 bg-[#63d72d] rounded-lg shadow-md overflow-hidden hover:transform hover:translate-y-[-30px]">
+        <div class="p-4">
+          <h2 class="text-xl font-bold text-white mt-16 text-center">Comedy</h2>
+        </div>
+      </div> 
     </div>
-    <div class="card w-full md:w-56 bg-[#63d72d]  rounded-lg shadow-md overflow-hidden">
-      
-      <div class="p-4">
-        <h2 class="text-xl font-bold text-white mt-16 text-center">Comedy</h2>
-      
-      </div>
-    </div>
-  </div>
-</section>
-
-
-
-
-
-
-
+  </section>
 </template>
+
 
 <script setup>
 
